1fdb69edb1ca5728edaffced323d9bbfe4bf91e3
Rois Admaja <43290237+Admaja-is-Admj@users.noreply.github.com>
2018-11-26 15:20:42 +0700

    membuat class Items dan mengisi atribut nama:String, harga:double, jumlah:int

---
+++ test/Items.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Items {
+    private String nama;
+    private double harga;
+    private int  jumlah;
+    
+}
