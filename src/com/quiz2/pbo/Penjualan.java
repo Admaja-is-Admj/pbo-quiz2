b56a05821aa2243d35d16f8443297e8c31d76bc4
Rois Admaja <43290237+Admaja-is-Admj@users.noreply.github.com>
2018-11-27 21:27:32 +0700

    Penjualan: menambahkan atribut

--- src/com/quiz2/pbo/Penjualan.java
+++ src/com/quiz2/pbo/Penjualan.java
@@ -5,10 +5,15 @@
  */
 package com.quiz2.pbo;
 
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author user
  */
 public class Penjualan {
+
+    private double total = 0;
+    private DefaultTableModel tableModel = new DefaultTableModel();
 
 }
