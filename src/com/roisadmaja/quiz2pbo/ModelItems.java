1c579fa7632ef8f355579747996a2acf1e88a91d
Rois Admaja <43290237+Admaja-is-Admj@users.noreply.github.com>
2018-11-30 20:20:36 +0700

    ModelItems : menambahkan Komentar

--- src/com/roisadmaja/quiz2pbo/ModelItems.java
+++ src/com/roisadmaja/quiz2pbo/ModelItems.java
@@ -9,34 +9,43 @@ package com.roisadmaja.quiz2pbo;
  *
  * @author user
  */
+// class ModelItems merupakan class untuk memodelkan item
 public class ModelItems {
+//atribut nama untuk mendeklarasikan nama
 
     private String nama;
+//atribut harga untuk mendeklarasikan harga
     private double harga;
+//konstruktor merupakan method untuk mengeksekusi
 
     public ModelItems(String nama, double harga) {
-        this.nama = nama;
-        this.harga = harga;
+        this.nama = nama; //inputan nama akan masuk ke atribut nama
+        this.harga = harga;//inputan harga akan masuk ke atribut harga
     }
 
+    //method getNama berguna untuk mengambil nama
     public String getNama() {
-        return nama;
+        return nama; //return nama untuk mengembalikan nilai dari nama
     }
 
+    //setNama untuk mengatur inputan nama
     public void setNama(String nama) {
-        this.nama = nama;
+        this.nama = nama;//mendeklarasikan inputan nama
     }
 
+    //getharga untuk mengambil value dari harga
     public double getHarga() {
-        return harga;
+        return harga; // mengembalikan nilai harga
     }
 
+    //setHarga untuk mengatur inputan harga
     public void setHarga(double harga) {
-        this.harga = harga;
+        this.harga = harga;//mendeklarasikan inputan harga
     }
 
+    //mengoveride class ModelItems
     @Override
     public String toString() {
-        return this.nama;
+        return this.nama; //mengembalikan nilai nama
     }
 }
